4d86adc7eb818094b14687f6ac9ae0c4a4470472
hao.yan <hirogen317@gmail.com>
2020-10-16 01:42:36 +0900

    simple example of v-model object

--- src/components/login_form1/index.stories.js
+++ src/components/login_form1/index.stories.js
@@ -4,5 +4,14 @@ export default { title: 'components/LoginForm1' };
 
 export const Text = () => ({
   components: { LoginForm1 },
-  template: '<LoginForm1  />',
+  template: '<div><LoginForm1 v-model="user" /> {{user}}</div>',
+  data() {
+    return {
+      user: {
+        name: '111',
+        email: '111',
+        password: '111'
+      }
+    }
+  }
 });
--- src/components/login_form1/index.vue
+++ src/components/login_form1/index.vue
@@ -1,14 +1,26 @@
 <template>
   <div>
-    <LabeledInput label="Username"  />
-    <LabeledInput label="Email" />
-    <LabeledInput label="Password" />
+    <LabeledInput label="Username" :value="value.name" @input="emitValue => update('name', emitValue)" />
+    <LabeledInput label="Email" :value="value.email" @input="emitValue => update('email', emitValue)" />
+    <LabeledInput label="Password" :value="value.password" @input="emitValue => update('password', emitValue)" />
     <q-btn />
   </div>
 </template>
 <script>
+import LabeledInput from '../labeled_input';
 export default {
   name: "LoginForm1",
-
+  components:{
+    LabeledInput
+  },
+  props: ['value'],
+  data() {
+    return {}
+  },
+  methods: {
+    update: function (key, emitValue) {
+      this.$emit('input', { ...this.value, [key]: emitValue})
+    }
+  }
 }
 </script>
