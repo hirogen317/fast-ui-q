185498e334785bc3ec4fe4f91cb793970d4083be
hao.yan <hirogen317@gmail.com>
2020-10-15 23:59:00 +0900

    Add some vendor icons

--- src/components/vendor_button/index.vue
+++ src/components/vendor_button/index.vue
@@ -1,12 +1,42 @@
 <template>
   <div>
-    <q-icon name="fas fa-search" color="black" />
-    <q-icon name="search" color="black" />
-    <q-btn >
+    <q-btn>
       <template v-slot:default >
         <q-icon name="fab fa-twitter" color="black" size="xs" /> &nbsp;Login with Twitter
       </template>
     </q-btn>
+
+    <q-btn >
+      <template v-slot:default >
+        <q-icon name="fab fa-google" color="black" size="xs" /> &nbsp;Login with Google
+      </template>
+    </q-btn>
+
+
+    <q-btn >
+      <template v-slot:default >
+        <q-icon name="fab fa-facebook" color="black" size="xs" /> &nbsp;Login with Google
+      </template>
+    </q-btn>
+
+    <q-btn >
+      <template v-slot:default >
+        <q-icon name="fab fa-github" color="black" size="xs" /> &nbsp;Login with Github
+      </template>
+    </q-btn>
+
+    <q-btn >
+      <template v-slot:default >
+        <q-icon name="fab fa-weibo" color="black" size="xs" /> &nbsp;Login with Weibo
+      </template>
+    </q-btn>
+
+    <q-btn >
+      <template v-slot:default >
+        <q-icon name="fab fa-weixin" color="black" size="xs" /> &nbsp;Login with Weixin
+      </template>
+    </q-btn>
+
   </div>
 
 </template>
